1833fa95ab4146d69c9915abe28ded662a0bf90c
lucmarbay <lukimartinezbayon@gmail.com>
2019-11-20 13:01:05 +0100

    Modificado el userController y la vista para que muestre los roles en el formulario

--- src/main/java/com/luciano/AppGym/controller/UserController.java
+++ src/main/java/com/luciano/AppGym/controller/UserController.java
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.luciano.AppGym.entity.User;
+import com.luciano.AppGym.repository.RoleRepository;
 import com.luciano.AppGym.service.UserService;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	RoleRepository roleRepository;
 	
 	@Autowired
 	UserService userService;
@@ -20,7 +25,10 @@ public class UserController {
 	
 	@GetMapping("/userForm")
 	public String getUserForm(Model model) {
+		model.addAttribute("userForm", new User());
 		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("listTab","active");
 		return "user-form/user-view";
 	}	
 }
