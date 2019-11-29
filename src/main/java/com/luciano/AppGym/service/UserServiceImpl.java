b107a1b63995444c6b42457eb6c700ec0987fadb
lucmarbay <lukimartinezbayon@gmail.com>
2019-11-29 14:30:27 +0100

    Añadido Spring Security con el sistema de roles

--- src/main/java/com/luciano/AppGym/service/UserServiceImpl.java
+++ src/main/java/com/luciano/AppGym/service/UserServiceImpl.java
@@ -3,6 +3,10 @@ package com.luciano.AppGym.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.luciano.AppGym.dto.ChangePasswordForm;
@@ -11,8 +15,12 @@ import com.luciano.AppGym.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService{
+
 	@Autowired
 	UserRepository repository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Override
 	public Iterable<User> getAllUsers() {
@@ -31,12 +39,14 @@ public class UserServiceImpl implements UserService{
 		if (user.getConfirmPassword() == null || user.getConfirmPassword().isEmpty()) {
 			throw new Exception("Confirm Password es obligatorio");
 		}
-
+		
 		if ( !user.getPassword().equals(user.getConfirmPassword())) {
 			throw new Exception("Password y Confirm Password no son iguales");
 		}
 		return true;
 	}
+
+
 	@Override
 	public User createUser(User user) throws Exception {
 		if (checkUsernameAvailable(user) && checkPasswordValid(user)) {
@@ -47,7 +57,7 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User getUserById(Long id) throws Exception {
-		return repository.findById(id).orElseThrow(() -> new Exception("El usuario para editar no existe."));
+		return repository.findById(id).orElseThrow(() -> new Exception("El usuario no existe."));
 	}
 
 	@Override
@@ -56,7 +66,7 @@ public class UserServiceImpl implements UserService{
 		mapUser(fromUser, toUser);
 		return repository.save(toUser);
 	}
-
+	
 	/**
 	 * Map everythin but the password.
 	 * @param from
@@ -69,31 +79,47 @@ public class UserServiceImpl implements UserService{
 		to.setEmail(from.getEmail());
 		to.setRoles(from.getRoles());
 	}
-	
-	public void deleteUser(Long id) throws Exception {
-		User user = repository.findById(id)
-				.orElseThrow(() -> new Exception("UsernotFound in deleteUser -"+this.getClass().getName()));
 
+	@Override
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	public void deleteUser(Long id) throws Exception {
+		User user = getUserById(id);
 		repository.delete(user);
 	}
+
+	
 	
 	@Override
 	public User changePassword(ChangePasswordForm form) throws Exception {
 		User user = getUserById(form.getId());
-
-		if ( !user.getPassword().equals(form.getCurrentPassword())) {
+		
+		if ( !isLoggedUserADMIN() && !user.getPassword().equals(form.getCurrentPassword())) {
 			throw new Exception ("Current Password invalido.");
 		}
-
+		
 		if( user.getPassword().equals(form.getNewPassword())) {
 			throw new Exception ("Nuevo debe ser diferente al password actual.");
 		}
-
+		
 		if( !form.getNewPassword().equals(form.getConfirmPassword())) {
 			throw new Exception ("Nuevo Password y Current Password no coinciden.");
 		}
-
-		user.setPassword(form.getNewPassword());
+		
+		String encodePassword = bCryptPasswordEncoder.encode(form.getNewPassword());
+		user.setPassword(encodePassword);
 		return repository.save(user);
+	}
+	
+	private boolean isLoggedUserADMIN() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserDetails loggedUser = null;
+		if (principal instanceof UserDetails) {
+			loggedUser = (UserDetails) principal;
+		
+			loggedUser.getAuthorities().stream()
+					.filter(x -> "ADMIN".equals(x.getAuthority() ))      
+					.findFirst().orElse(null); //loggedUser = null;
+		}
+		return loggedUser != null ?true :false;
 	}
 }
